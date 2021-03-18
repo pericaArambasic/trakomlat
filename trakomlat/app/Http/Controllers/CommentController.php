bbf2bd103a04251daf0180b9dd2a77d9d0dd909c
Perica <=>
2021-03-18 15:53:06 +0100

    add rating controller func

--- trakomlat/app/Http/Controllers/CommentController.php
+++ trakomlat/app/Http/Controllers/CommentController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Comment;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class CommentController extends Controller
 {
--- trakomlat/app/Http/Controllers/RatingController.php
+++ trakomlat/app/Http/Controllers/RatingController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Rating;
 use Illuminate\Http\Request;
 
 class RatingController extends Controller
 {
-    //
+    public function addRating(Request $request)
+    {
+        $rating = new Rating;
+        $rating->rating = $request->rating;
+        $rating->video = $request->video;
+        $rating->user = $request->user;
+        $rating->save();
+
+        return response()->json([
+            "message" => "rating added"
+        ],201);
+    }
+
+    public function changeRating(Request $request, $id)
+    {
+        $rating = Rating::where('id',$id);
+    }
 }
